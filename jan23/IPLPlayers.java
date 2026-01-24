75600bbc71ed108184b120857b7d5dc9e4c73867
Pratikshasapkal <130816927+Pratikshasapkal@users.noreply.github.com>
2026-01-24 15:31:39 +0530

    Add files via upload

---
+++ jan23/IPLPlayers.java
@@ -0,0 +1,26 @@
+package com.tka.jan23;
+
+import java.util.ArrayList;
+
+public class IPLPlayers {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Players> players = new ArrayList<>();
+		
+		players.add(new Players(18, "Virat Kohli", 7263, 4, "RCB"));
+		players.add(new Players(45, "Rohit Sharma", 6211, 15, "MI"));
+		players.add(new Players(7, "MS Dhoni", 5082, 0, "CSK"));
+		players.add(new Players(33, "Hardik Pandya", 2309, 53, "MI"));
+		players.add(new Players(77, "Shubman Gill", 2790, 0, "GT"));
+		players.add(new Players(63, "Suryakumar Yadav", 3249, 0, "MI"));
+
+		
+		for(Players p : players) {
+			
+			System.out.println(p.jerseyNum + " " + p.name + " " + p.runs + " " + p.wickets + " " + p.teamName );
+		}
+
+	}
+
+}
